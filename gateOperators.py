a9e821661d39b166c1a7da4da3799c9feebd019f
Casey Duckering <cduckering@gmail.com>
2017-11-09 12:27:12 -0800

    Change global phase of rotation gates, update other gates

--- gateOperators.py
+++ gateOperators.py
@@ -8,11 +8,13 @@ complexDtype = np.complex
 intDtype = np.int
 
 
-I1 = sparse.identity(2, dtype=intDtype)
-X = np.array([[0,1],[1,0]], dtype=intDtype)
-Y = np.array([[0,-1j],[1j,0]], dtype=complexDtype)
-Z = np.array([[1,0],[0,-1]], dtype=intDtype)
-H = np.array([[1,1],[1,-1]], dtype=intDtype)  # *INV_SQ2
+PGen = lambda theta: np.exp(1j*theta) * np.identity(1, dtype=complexDtype)
+
+I1 = np.identity(2, dtype=intDtype)
+X = np.array([[0,1j],[1j,0]], dtype=complexDtype)
+Y = np.array([[0,1],[-1,0]], dtype=complexDtype)
+Z = np.array([[-1j,0],[0,1j]], dtype=complexDtype)
+H = np.array([[1,1],[1,-1]], dtype=intDtype)
 S = np.array([[1,0],[0,1j]], dtype=complexDtype)
 Sd = np.array([[1,0],[0,-1j]], dtype=complexDtype)
 T = np.array([[1,0],[0,(1+1j)/np.sqrt(2)]], dtype=complexDtype)
@@ -22,7 +24,8 @@ RxGen = lambda theta: np.array([[np.cos(theta/2),1j*np.sin(theta/2)],
                                 [1j*np.sin(theta/2),np.cos(theta/2)]], dtype=complexDtype)
 RyGen = lambda theta: np.array([[np.cos(theta/2),np.sin(theta/2)],
                                 [-np.sin(theta/2),np.cos(theta/2)]], dtype=complexDtype)
-RzGen = lambda theta: np.array([[1,0],[0,np.exp(1j*theta)]], dtype=complexDtype)
+RzGen = lambda theta: np.array([[np.exp(-0.5j*theta),0],
+                                [0,np.exp(0.5j*theta)]], dtype=complexDtype)
 
 CRzGen = lambda theta: np.array([[1,0,0,0],
                                  [0,1,0,0],
@@ -37,7 +40,7 @@ CCRzGen = lambda theta: np.array([[1,0,0,0,0,0,0,0],
                                   [0,0,0,0,0,0,1,0],
                                   [0,0,0,0,0,0,0,np.exp(1j*theta)]], dtype=complexDtype)
 
-I2 = sparse.identity(4, dtype=intDtype)
+I2 = np.identity(4, dtype=intDtype)
 CX = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]], dtype=intDtype)
 SWAP = np.array([[1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]], dtype=intDtype)
 
